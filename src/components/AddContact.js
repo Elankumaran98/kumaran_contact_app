4fc4e02f9391e6626db67d4d8a80bd715075e049
Elankumaran98 <siva98kumarane@gmail.com>
2021-08-22 10:09:24 +0530

    Kumaran Contact App created

---
+++ src/components/AddContact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddContact=()=>{
+    return(
+        <div className='ui header menu'>
+            <div className='ui header center'>
+                <h2>Contact Manager</h2>
+            </div>
+        </div>
+    );
+}
+export default AddContact;
---
+++ src/components/ContactCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactCard=()=>{
+    return(
+        <div className='ui header menu'>
+            <div className='ui header center'>
+                <h2>Contact Manager</h2>
+            </div>
+        </div>
+    );
+}
+export default ContactCard;
