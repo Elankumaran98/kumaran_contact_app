c0ae9e28ccbf5dbf0f25915ff66b2b4e22a232b5
Elankumaran98 <siva98kumarane@gmail.com>
2021-08-31 19:41:22 +0530

    bug fixed

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import theme from "../images/theme.jpg";
 
 const ContactCard = (props) => {
-  // eslint-disable-next-line no-unused-vars
   const { id, name, email } = props.contact;
   return (
     <div className="item">
@@ -17,7 +16,7 @@ const ContactCard = (props) => {
             },
           }}
         >
-          <div className="header">{name}</div>
+          <div className="header"> {name}</div>
           <div className="header">{email}</div>
         </Link>
         <i
